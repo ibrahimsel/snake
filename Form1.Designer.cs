bf7954c8eaac4d141bbdda72fbb46cbe576319ad
Ibrahim <63649222+ibrahimsel@users.noreply.github.com>
2020-12-08 13:36:09 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.GameLoop = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.minuteLabel = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(144, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(660, 360);
+            this.panel1.TabIndex = 0;
+            // 
+            // GameLoop
+            // 
+            this.GameLoop.Enabled = true;
+            this.GameLoop.Tick += new System.EventHandler(this.GameLoop_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(345, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Snake Game";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(795, 58);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(33, 13);
+            this.timeLabel.TabIndex = 2;
+            this.timeLabel.Text = "Time:";
+            // 
+            // minuteLabel
+            // 
+            this.minuteLabel.AutoSize = true;
+            this.minuteLabel.Location = new System.Drawing.Point(835, 58);
+            this.minuteLabel.Name = "minuteLabel";
+            this.minuteLabel.Size = new System.Drawing.Size(25, 13);
+            this.minuteLabel.TabIndex = 3;
+            this.minuteLabel.Text = "00: ";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.AutoSize = true;
+            this.secondsLabel.Location = new System.Drawing.Point(855, 58);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(19, 13);
+            this.secondsLabel.TabIndex = 4;
+            this.secondsLabel.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(827, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "HeadX: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(827, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "HeadY:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(830, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "food";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(830, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(980, 622);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.secondsLabel);
+            this.Controls.Add(this.minuteLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer GameLoop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label minuteLabel;
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+
+
+/* Tasks: 
+ * Self Collision
+ * get another spawn point for food if there is a snake particle
+ */
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        readonly Random rand = new Random();
+        readonly Label food = new Label();
+        readonly Label[] snake = new Label[2376];
+        // readonly (int, int)[] placesVisited = new (int, int)[2376];
+        List<(int, int)> placesVisited = new List<(int, int)>();
+
+        public int x, xTemp;
+        public int y, yTemp;
+        public int particleWidth = 10;
+        public int particleHeight = 10;
+        public int panelWidth = 660;
+        public int panelHeight = 360;
+        public int headPositionX = 200;  // snake[0] is our snake's head
+        public int headPositionY = 200;
+        public int snakeVelocity = 10;  // have to assign this to radiobutton
+        public int snakeSize = 5;  // The variable to hold how many particles our snake have
+        public string snakeDirection = "right";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fillTheList()
+        {
+            for (int i = 0; i < snakeSize; i++)
+            {
+                placesVisited.Add((i, i + i));
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //GameLoop.Enabled = false;
+            //fillTheList();
+            //label1.Text = placesVisited[4].ToString();
+            SpawnTheSnake();
+            SpawnFoodParticle();
+        }
+
+        private Label AddSnakeParticle(int x, int y)
+        {
+            Label snake = new Label
+            {
+                Size = new Size(particleWidth, particleHeight),
+                Location = new Point(x, y),
+                BackColor = Color.Green
+            };
+            panel1.Controls.Add(snake);
+            return snake;
+        }
+
+        private void SpawnTheSnake()
+        {
+            for (int i = 0; i < snakeSize; i++)
+            {
+                snake[i] = AddSnakeParticle(headPositionX - i * particleWidth, headPositionY);  // We use - (i * particleWidth) to add labels next to the head, then each other
+            }
+        }
+
+        private void Grow()
+        {
+            snake[snakeSize] = AddSnakeParticle(snake[snakeSize - 1].Location.X, snake[snakeSize - 1].Location.Y);
+            snakeSize++;
+        }
+
+        private void SpawnFoodParticle()
+        {
+            int foodX = rand.Next(0, panelWidth - particleWidth);
+            int foodY = rand.Next(0, panelHeight - particleHeight);
+
+            food.Size = new Size(particleWidth, particleHeight);
+            food.Location = new Point(foodX + (snakeVelocity - (foodX % snakeVelocity)), foodY + (snakeVelocity - (foodY % snakeVelocity)));  // We need to spawn the food in places which our snake is able to visit...
+            food.BackColor = Color.DarkRed;                                                                                                       // So we only produce random numbers which are multiplies of our snake's velocity
+            panel1.Controls.Add(food);
+        }
+
+        private void RemoveFoodParticle()
+        {
+            panel1.Controls.Remove(food);
+        }
+
+
+        private bool FoodCollision()
+        {
+            if ((snake[0].Location.X == food.Location.X) && (snake[0].Location.Y == food.Location.Y))
+            {
+                RemoveFoodParticle();
+                SpawnFoodParticle();
+                return true;
+            }
+            return false;
+        }
+
+        private bool WallCollision()
+        {
+            if (snake[0].Location.X < 0 || snake[0].Location.X > 650 || snake[0].Location.Y < 0 || snake[0].Location.Y > 350)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool SelfCollision()
+        {
+            for (int i = 0; i < snakeSize; i++)
+            {
+
+            }
+            return false;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    snakeDirection = "right";
+                    break;
+
+                case Keys.Left:
+                    snakeDirection = "left";
+                    break;
+
+                case Keys.Up:
+                    snakeDirection = "up";
+                    break;
+
+                case Keys.Down:
+                    snakeDirection = "down";
+                    break;
+
+            }
+        }
+
+        private void GameLoop_Tick(object sender, EventArgs e)
+        {
+            if (FoodCollision())
+                Grow();
+
+            if (WallCollision())
+            {
+                GameLoop.Enabled = false;
+                MessageBox.Show("Game Over");
+            }
+
+            if (SelfCollision())
+                ;
+
+            label2.Text = "headX: " + snake[0].Location.X.ToString();
+            label3.Text = "headY: " + snake[0].Location.Y.ToString();
+            label4.Text = "foodY: " + food.Location.X.ToString();
+            label5.Text = "foodY: " + food.Location.Y.ToString();
+
+
+            switch (snakeDirection)
+            {
+                case "down":
+                    #region                
+                    for (int i = 0; i < snakeSize; i++)
+                    {
+                        x = snake[i].Location.X;
+                        y = snake[i].Location.Y;
+
+                        if (i == 0)
+                        {
+                            snake[i].Location = new Point(x, y + snakeVelocity);
+                            xTemp = x;
+                            yTemp = y;
+                        }
+
+                        else
+                        {
+                            snake[i].Location = new Point(xTemp, yTemp);
+                            xTemp = x;
+                            yTemp = y;
+                        }
+                    }
+                    #endregion
+                    break;
+
+                case "up":
+                    #region                   
+                    for (int i = 0; i < snakeSize; i++)
+                    {
+                        x = snake[i].Location.X;
+                        y = snake[i].Location.Y;
+
+                        if (i == 0)
+                        {
+                            snake[i].Location = new Point(x, y - snakeVelocity);
+                            xTemp = x;
+                            yTemp = y;
+
+                        }
+
+                        else
+                        {
+                            snake[i].Location = new Point(xTemp, yTemp);
+                            xTemp = x;
+                            yTemp = y;
+
+                        }
+                    }
+                    #endregion
+
+                    break;
+
+                case "left":
+                    #region                                       
+                    for (int i = 0; i < snakeSize; i++)
+                    {
+                        x = snake[i].Location.X;
+                        y = snake[i].Location.Y;
+
+                        if (i == 0)
+                        {
+                            snake[i].Location = new Point(x - snakeVelocity, y);
+                            xTemp = x;
+                            yTemp = y;
+                        }
+
+                        else
+                        {
+                            snake[i].Location = new Point(xTemp, yTemp);
+                            xTemp = x;
+                            yTemp = y;
+                        }
+                    }
+                    #endregion
+                    break;
+
+                case "right":
+                    #region
+                    for (int i = 0; i < snakeSize; i++)
+                    {
+                        x = snake[i].Location.X;
+                        y = snake[i].Location.Y;
+
+                        if (i == 0)
+                        {
+                            snake[i].Location = new Point(x + snakeVelocity, y);
+                            xTemp = x;
+                            yTemp = y;
+                        }
+                        else
+                        {
+                            snake[i].Location = new Point(xTemp, yTemp);
+                            xTemp = x;
+                            yTemp = y;
+                        }
+                    }
+                    #endregion
+                    break;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
